91559709c47eee1c0f3abcc45899d14c9806575c
csandoval63 <csandoval63@student.rcc.edu>
2016-02-25 11:51:59 -0800

    Added files via upload

---
+++ Lab/Hello World v2/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Christian Sandoval
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose:  Our first program
+ * V2
+ */
+
+//System Libraries
+
+#include <iostream>
+
+//using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv)
+{
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    std::cout<<"Hello World"<<std::endl;
+
+    //Exit Stage Right!
+    return 0;
+}
