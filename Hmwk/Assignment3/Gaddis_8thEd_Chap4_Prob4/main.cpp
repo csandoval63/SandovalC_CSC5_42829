f6a6f4e7114d8c6117bd87ae696b830c7ad3fa35
DrGiggleZ Giggles <DrGiggleZ Giggles>
2016-03-28 19:12:01 -0700

    Signed-off-by: DrGiggleZ Giggles <DrGiggleZ Giggles>

--- Hmwk/Assignment3/Gaddis_8thEd_Chap4_Prob4/main.cpp
+++ Hmwk/Assignment3/Gaddis_8thEd_Chap4_Prob4/main.cpp
@@ -12,27 +12,47 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    //declare variables
-    //const char SIZE = 3;
-    float recLen1, reclen2, recWid1, recWid2,
+    const int SIZE = 5;
+    char uniM[SIZE];
+    float recLen1, recLen2, recWid1, recWid2,
             recAr1, recAr2;
-    //char uniM;
     
     //Ask user for units of measure used
-    //cout << "What are the rectangles measured in (abreviated form)?" << endl;
-    //cin >> uniM;
+    cout << "What are the rectangles measured in (abreviated form)?" << endl;
+    cin >> uniM;
     
     //Ask user for input info for rectangle 1
-    cout << "What is the length of rectangle 1" << endl;
+    cout << "What is the length of first rectangle" << endl;
     cin >> recLen1;
-    cout << "What is the width of rectangle 1" << endl;
+    cout << "What is the width of first rectangle" << endl;
     cin >> recWid1;
     
     //Ask user for input info for rectangle 2
-    cout << "What is the length of rectangle 2" << endl;
-    cin >> recLen1;
-    cout << "What is the width of rectangle 2" << endl;
-    cin >> recWid1;
+    cout << "What is the length of second rectangle" << endl;
+    cin >> recLen2;
+    cout << "What is the width of second rectangle" << endl;
+    cin >> recWid2;
+    
+    //Equation
+    recAr1 = recLen1 * recWid1;
+    recAr2 = recLen2 * recWid2;
+    
+    //map output results
+    if(recAr1 < recAr2)
+    {
+        cout << "Area of First rectangle " << recAr1 << uniM
+             << " is less than Area of second rectangle " << recAr2 << uniM << endl;
+    }
+    else if(recAr1 > recAr2)
+    {
+        cout << "Area of First rectangle " << recAr1 << uniM 
+             << " is greater than Area of second rectangle " << recAr2 << uniM << endl;
+    }
+    else if(recAr1 == recAr2)
+    {
+        cout << "Area of First rectangle " << recAr1 << uniM
+             << " is equal than Area of second rectangle " << recAr2 << uniM << endl;
+    }
     
     return 0;
 }
--- Hmwk/Assignment3/Gaddis_8thEd_Chap4_Prob4/nbproject/Makefile-variables.mk
+++ Hmwk/Assignment3/Gaddis_8thEd_Chap4_Prob4/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
 CND_ARTIFACT_NAME_Debug=gaddis_8thed_chap4_prob4
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/gaddis_8thed_chap4_prob4
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/gaddis_8thed_chap4_prob4
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
 CND_PACKAGE_NAME_Debug=gaddis8thedchap4prob4.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/gaddis8thedchap4prob4.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/gaddis8thedchap4prob4.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
 CND_ARTIFACT_NAME_Release=gaddis_8thed_chap4_prob4
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/gaddis_8thed_chap4_prob4
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/gaddis_8thed_chap4_prob4
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
 CND_PACKAGE_NAME_Release=gaddis8thedchap4prob4.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/gaddis8thedchap4prob4.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/gaddis8thedchap4prob4.tar
 #
 # include compiler specific variables
 #
