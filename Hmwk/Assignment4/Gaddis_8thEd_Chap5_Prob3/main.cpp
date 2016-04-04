6dd9f29ab06628f87b49f6a056db8baddfb24ae8
csandoval63 <csandoval63@student.rcc.edu>
2016-04-03 21:33:48 -0700

    Signed-off-by: csandoval63 <csandoval63@student.rcc.edu>

---
+++ Hmwk/Assignment4/Gaddis_8thEd_Chap5_Prob3/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Christian Sandoval
+ * Purpose: Rising ocean Levels
+ * Created on April 3, 2016, 9:02 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    //Declare variables
+    float oRisng, sumOyrs, years;
+
+    cout << "Ocean level over span of 25 years will rise: " << endl;
+
+    for(years = 1, oRisng = 1.5; years <= 25; years++ )
+    {
+        sumOyrs = oRisng * years;
+        cout << "Year: " << years << " Ocean level risen by " << sumOyrs << "mm"<< endl;
+    }
+
+    cout << "At 25 years ocean will have risen by: " << sumOyrs << "mm" << endl;
+
+    return 0;
+}
