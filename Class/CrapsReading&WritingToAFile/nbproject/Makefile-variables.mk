07bb7bd80b63ce1acf003c2c9e745778d776ca8d
csandoval63 <csandoval63@student.rcc.edu>
2016-05-04 07:53:01 -0700

    Signed-off-by: csandoval63 <csandoval63@student.rcc.edu>

--- Class/CrapsReading&WritingToAFile/nbproject/Makefile-variables.mk
+++ Class/CrapsReading&WritingToAFile/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
 CND_ARTIFACT_NAME_Debug=crapsreading_writingtoafile
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/crapsreading_writingtoafile
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/crapsreading_writingtoafile
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
 CND_PACKAGE_NAME_Debug=crapsreading&writingtoafile.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/crapsreading&writingtoafile.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/crapsreading&writingtoafile.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
 CND_ARTIFACT_NAME_Release=crapsreading_writingtoafile
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/crapsreading_writingtoafile
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/crapsreading_writingtoafile
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
 CND_PACKAGE_NAME_Release=crapsreading&writingtoafile.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/crapsreading&writingtoafile.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/crapsreading&writingtoafile.tar
 #
 # include compiler specific variables
 #
